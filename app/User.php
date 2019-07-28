20a34550df077b6aa237c5ff3618e3529503a957
Leo <poussin6494@hotmail.com>
2019-07-28 21:15:14 +0200

    Generate User and questions

--- app/User.php
+++ app/User.php
@@ -40,9 +40,4 @@ class User extends Authenticatable
     {
         return $this->hasMany(Question::class);
     }
-    public function setTittleAttribute($value)
-    {
-        $this->attributes['title'] = $value;
-        $this->attributes['slug'] = str_slug($value);
-    }
 }
--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -7,6 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Question::class, function (Faker $faker) {
     return [
-        'title'=>rtrim($faker->sentence(rand(5,10),"."))
+        // rand genère des nombres aléatoires donc rand 5 à 10
+        'title'=>rtrim($faker->sentence(rand(5,10),".")),//enlève les points 
+        'body'=>$faker->paragraph(rand(3,7),true),// avec des sauts de lignes
+        'views'=>rand(0,10),
+        'answers'=>rand(0,10),
+        'votes'=>rand(-3,10)
     ];
 });
