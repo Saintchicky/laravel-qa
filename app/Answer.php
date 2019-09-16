6dfd2fca54c3f6f1c9d3ddc06c28fd4fef6df1ef
Leo <poussin6494@hotmail.com>
2019-09-16 21:52:35 +0200

    Delete

--- app/Answer.php
+++ app/Answer.php
@@ -7,31 +7,37 @@ use Illuminate\Database\Eloquent\Model;
 class Answer extends Model
 {
     protected $fillable = ['body', 'user_id'];
+    
     public function question()
     {
         return $this->belongsTo(Question::class);
     }
+
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
     public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
     }
-    // Eloqent Events
+    
     public static function boot()
-    {   
-        // On appelle la méthode existante en tant que parent
+    {
         parent::boot();
-        static::created(function($answer){
-            $answer->question->increment('answers_count');
-            $answer->question->save();
+
+        static::created(function ($answer) {
+            $answer->question->increment('answers_count');                     
+        });        
+
+        static::deleted(function ($answer) {
+            $answer->question->decrement('answers_count');
         });
     }
+
     public function getCreatedDateAttribute()
     {
-        // Pour afficher en jour
         return $this->created_at->diffForHumans();
     }
 }
