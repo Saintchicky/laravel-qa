eacc8f2f9e544409d2e2845b0a630082df5c0358
Saintchicky <poussin6494@hotmail.com>
2019-10-20 17:19:00 +0200

    Ajax begin

--- resources/views/answers/_answer.blade.php
+++ resources/views/answers/_answer.blade.php
@@ -1,29 +1,36 @@
+<answer :answer="{{$answer}}" inline-template>
 <div class="media post">
     @include ('shared._vote', [
         'model' => $answer
     ])
-    
     <div class="media-body">
-        {!! $answer->body_html !!}
-        <div class="row">
-            <div class="col-4">
-                <div class="ml-auto">
-                    @can ('update', $answer)
-                        <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                    @endcan
-                    @can ('delete', $answer)
-                        <form class="form-delete" method="post" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
-                            @method('DELETE')
-                            @csrf
-                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                        </form>
-                    @endcan
+        <form v-if="editing"> <!--c'est comme if(editing)-->
+            Edit answer form
+            <button @click="editing = false" type="button">Update</button>
+        </form>
+        <div v-else><!--Doit être utilisé sur le même niveau-->
+            {!! $answer->body_html !!}
+            <div class="row">
+                <div class="col-4">
+                    <div class="ml-auto">
+                        @can ('update', $answer)
+                            <a @click.prevent="editing = true" class="btn btn-sm btn-outline-info">Edit</a>
+                        @endcan
+                        @can ('delete', $answer)
+                            <form class="form-delete" method="post" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                            </form>
+                        @endcan
+                    </div>
                 </div>
-            </div>
-            <div class="col-4"></div>
-            <div class="col-4">
-                <user-info :model="{{ $answer }}" label="Answered"></user-info>
-            </div>
+                <div class="col-4"></div>
+                <div class="col-4">
+                    <user-info :model="{{ $answer }}" label="Answered"></user-info>
+                </div>
+            </div>    
         </div>                            
     </div>
 </div>
+</answer>
