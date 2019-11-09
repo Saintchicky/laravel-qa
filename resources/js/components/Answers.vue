5e7084f510cfc6dbea70c3cff5322fe4be84f276
Leo <poussin6494@hotmail.com>
2019-11-09 23:12:09 +0100

    For vue js

---
+++ resources/js/components/Answers.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row mt-4" v-cloak v-if="count"><!--S'ouvre que si on l'appelle grâce à l'instance associée -->
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h2>{{ title }}</h2>
+                    </div>
+                    <hr>
+                    <!-- Déclarer la clé -->
+                    <answer v-for="answer in answers"  :answer="answer" :key="answer.id"></answer>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import Answer from './Answer.vue';
+export default {
+    props:['answers','count'],
+    computed:{
+        title(){
+            return this.count + " " + (this.count > 1 ? 'Answers' : 'Answer');
+        }
+    },
+    components:{ Answer }
+}
+</script>
--- resources/views/answers/_answer.blade.php
+++ resources/views/answers/_answer.blade.php
@@ -1,35 +1,35 @@
 <answer :answer="{{$answer}}" inline-template>
-<div class="media post">
-    <vote :model="{{ $answer }}" name="answer"></vote>
-    <!--On met la méthode après prevent update-->
-    <div class="media-body">
-        <form v-if="editing" @submit.prevent="update"> <!--c'est comme if(editing)-->
-            <div class="form-group">
-                <textarea rows="10" v-model="body" class="form-control" required></textarea>
-            </div>
-            <button class="btn btn-primary" :disabled="isInvalid">Update</button>
-            <button class="btn btn-outline-secondary" @click="cancel" type="button">Cancel</button>
-        </form>
-        <div v-else><!--Doit être utilisé sur le même niveau-->
-            <div v-html="bodyHtml"></div>
-            <div class="row">
-                <div class="col-4">
-                    <div class="ml-auto">
-                        @can ('update', $answer)
-                            <button @click.prevent="edit" class="btn btn-sm btn-outline-info">Edit</button>
-                        @endcan
-                        @can ('delete', $answer)
-                        <button @click="destroy" class="btn btn-sm btn-outline-danger">Delete</button>
-                            
-                        @endcan
+    <div class="media post">
+        <vote :model="{{ $answer }}" name="answer"></vote>
+        <!--On met la méthode après prevent update-->
+        <div class="media-body">
+            <form v-if="editing" @submit.prevent="update"> <!--c'est comme if(editing)-->
+                <div class="form-group">
+                    <textarea rows="10" v-model="body" class="form-control" required></textarea>
+                </div>
+                <button class="btn btn-primary" :disabled="isInvalid">Update</button>
+                <button class="btn btn-outline-secondary" @click="cancel" type="button">Cancel</button>
+            </form>
+            <div v-else><!--Doit être utilisé sur le même niveau-->
+                <div v-html="bodyHtml"></div>
+                <div class="row">
+                    <div class="col-4">
+                        <div class="ml-auto">
+                            @can ('update', $answer)
+                                <button @click.prevent="edit" class="btn btn-sm btn-outline-info">Edit</button>
+                            @endcan
+                            @can ('delete', $answer)
+                            <button @click="destroy" class="btn btn-sm btn-outline-danger">Delete</button>
+                                
+                            @endcan
+                        </div>
                     </div>
-                </div>
-                <div class="col-4"></div>
-                <div class="col-4">
-                    <user-info :model="{{ $answer }}" label="Answered"></user-info>
-                </div>
-            </div>    
-        </div>                            
+                    <div class="col-4"></div>
+                    <div class="col-4">
+                        <user-info :model="{{ $answer }}" label="Answered"></user-info>
+                    </div>
+                </div>    
+            </div>                            
+        </div>
     </div>
-</div>
 </answer>
